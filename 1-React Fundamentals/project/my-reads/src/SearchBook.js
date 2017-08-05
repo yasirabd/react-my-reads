e55bcf8fe493f362c7a549523bd239683c22bd03
yasirabd <sir.yasirabd@gmail.com>
2017-08-06 06:33:05 +0700

    change close search link into a Link component

--- 1-React Fundamentals/project/my-reads/src/SearchBook.js
+++ 1-React Fundamentals/project/my-reads/src/SearchBook.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class SearchBook extends Component {
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link className='close-search' to='/'>Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
